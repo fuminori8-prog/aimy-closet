508d9f5e10ac8ed2c743cd6e337f15287e1039f8
appleuser <appleuser@MacBook-Air-M3.local>
2026-07-14 22:17:51 +0900

    Improve header and navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,13 +5,13 @@ function Header() {
     <header className="header">
       <div className="logo-area">
         <Link to="/" className="logo logo-link">
-          Aimy Closet
+            Aimy Closet
         </Link>
 
-        <p className="logo-description">
-          Aimy非公式アイテム図鑑・ガチャデータベース
-        </p>
-      </div>
+            <span className="logo-description">
+                Aimy非公式アイテム図鑑・ガチャデータベース
+            </span>
+    </div>
 
       <nav className="header-nav">
         <NavLink to="/item" className="nav-button">
