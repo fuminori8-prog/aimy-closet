5688f4bd7e6cc7027761eb5b9e815fbd3e525faf
Fuminori <fuminori8@gmail.com>
2026-09-11 21:09:01 +0900

    Add おそろい♡ゆめかわコーデ gacha

--- src/data/gachas.js
+++ src/data/gachas.js
@@ -1,3 +1,4 @@
+import osoroiYumeKaWaKode from './gachas/osoroi-yume-ka-wa-kode'
 import haitokuNoShisutaTsumibukakiInori from './gachas/haitoku-no-shisuta-tsumibukaki-inori'
 import bichiDeOKyujiShiChaiMasu from './gachas/bichi-de-o-kyuji-shi-chai-masu'
 import torokeruNittoNoYuwaku from './gachas/torokeru-nitto-no-yuwaku'
@@ -38,6 +39,7 @@ import kimiNoTokubetsu from './gachas/kimi-no-tokubetsu'
 import friendPoint from './gachas/friend-point'
 
 export const gachas = [
+  osoroiYumeKaWaKode,
   haitokuNoShisutaTsumibukakiInori,
   bichiDeOKyujiShiChaiMasu,
   torokeruNittoNoYuwaku,
